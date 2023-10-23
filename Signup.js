4826a0529e0149073535aaf4d0679f4f5a1330ab
Stephenmathew03 <136146712+Stephenmathew03@users.noreply.github.com>
2023-10-23 16:09:58 +0530

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <div className="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div className="row align-items-center g-lg-5 py-5">
+          <div className="col-md-10 mx-auto col-lg-5">
+            <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                />
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
+              <div className="checkbox mb-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Remember me
+                </label>
+              </div>
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Sign up
+              </button>
+              <hr className="my-4" />
+              <small className="text-body-secondary">
+                By clicking Sign up, you agree to the terms of use.
+              </small>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
